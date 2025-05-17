46451889930cb73fedd91a27400d3938f9852ffa
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-05-17 13:54:44 +0300

    added a hitbox component

---
+++ scripts/components/collision/hitbox_component.gd
@@ -0,0 +1,12 @@
+class_name HitboxComponent extends Area2D
+
+func attack():
+	var components = []
+	var areas = get_overlapping_areas()
+	for area in areas:
+		if area is HurtboxComponent:
+			components.append(area)
+	if components.size() > 0:
+		return components
+	else:
+		return null
