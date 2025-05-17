c3d8267c0ccd3857245c9ab81ecf3d1045ec370b
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-05-17 14:23:16 +0300

    added the rest of the icons to the dict

--- scripts/globals/globals.gd
+++ scripts/globals/globals.gd
@@ -2,8 +2,17 @@ extends Node
 
 const VERSION_NUM: String = "v0.0.1 DEV"
 
-var mouse_icons = { 
+var mouse_icons: Dictionary[String, Image] = { 
 	"mouse_circle_x_icon" = load("res://assets/images/crosshairs/crosshair052.png"),
+	"empty" = load("res://assets/images/crosshairs/crosshair001.png"),
+	"plus" = load("res://assets/images/crosshairs/crosshair002.png"),
+	"cross" = load("res://assets/images/crosshairs/crosshair005.png"),
+	"two_arrows" = load("res://assets/images/crosshairs/crosshair018.png"),
+	"square" = load("res://assets/images/crosshairs/crosshair032.png"),
+	"eye" = load("res://assets/images/crosshairs/crosshair140.png"),
+	"cancel" = load("res://assets/images/crosshairs/crosshair113.png"),
+	"triangle" = load("res://assets/images/crosshairs/crosshair100.png"),
+	"eye_arrows" = load("res://assets/images/crosshairs/crosshair058.png")
 }
 
 func change_mouse_icon(mouse_icon) -> void:
