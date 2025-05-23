b6da7b1ca8965118a8f75c8c6917dfe9a1497d31
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-05-23 17:12:37 +0300

    added new component

--- scripts/components/dialogue_component.gd
+++ scripts/components/dialogue_component.gd
@@ -8,18 +8,32 @@ class_name DialogueComponent extends Node2D
 @onready var dialogue_area: Area2D = $DialogueArea
 @onready var dialogue_area_shape: CollisionShape2D = $DialogueArea/CollisionShape2D
 
-func _ready():
+var default_scale: Vector2
+var tween: Tween
+
+func _ready() -> void:
+	default_scale = prompt.scale
 	prompt.position = prompt_offset
 	dialogue_area_shape.shape.radius = prompt_radius
 
 	dialogue_area.body_entered.connect(func(body):
 		if body is Player:
 			if prompt_visible:
-				prompt.visible = true
+				show_prompt()
 		)
 
 	dialogue_area.body_exited.connect(func(body):
 		if body is Player:
 			if prompt_visible:
-				prompt.visible = false
+				hide_prompt()
 		)
+
+func show_prompt() -> void:
+	prompt.visible = true
+	tween = create_tween()
+	tween.tween_property(prompt, "scale", default_scale, 0.3).from(Vector2(0, 0)).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+
+func hide_prompt() -> void:
+	tween = create_tween()
+	await tween.tween_property(prompt, "scale", Vector2(0, 0), 0.3).from(prompt.scale).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT).finished
+	prompt.visible = false
